53a173ef84da0ee1410c844ea1fddfd5341620bb
Raymond <raymondandwork@gmail.com>
2018-03-06 21:37:37 +1100

    add shared code directory and update bootstrap code

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 sudo tar -C /usr/local -xzf /vagrant/Software/go1.10.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
+echo "export PATH=$PATH:/usr/local/go/bin" >> /home/vagrant/.profile
