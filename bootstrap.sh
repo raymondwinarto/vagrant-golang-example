d49b8ba020a853c7dd06d2ded6c913f709337f88
Ray <rays_win@hotmail.com>
2018-03-07 20:37:49 +1100

    Provisioning as vagrant user

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo tar -C /usr/local -xzf /vagrant/Software/go1.10.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> /home/vagrant/.profile
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
